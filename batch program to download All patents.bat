bd410e794711c8424326b0045575dc505e9d39b0
latluri <atlurilaxminarayana@gmail.com>
2016-02-22 19:43:42 -0500

    Downloads patents

---
+++ batch program to download All patents.bat
@@ -0,0 +1,204 @@
+  
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071225.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071218.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071211.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071204.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071127.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071120.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071113.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071106.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071030.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071023.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071016.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071009.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg071002.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070925.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070918.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070911.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070904.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070828.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070821.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070814.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070807.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070731.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070724.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070717.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070710.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070703.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070626.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070619.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070612.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070605.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070529.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070522.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070515.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070508.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070501.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070424.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070417.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070410.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070403.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070327.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070320.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070313.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070306.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070227.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070220.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070213.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070206.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070130.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070123.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070116.zip"  
+ timeout /t 240 
+ 
+   
+ "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "http://patents.reedtech.com/downloads/GrantRedBookText/2007/ipg070109.zip"  
+ timeout /t 240 
+ 
